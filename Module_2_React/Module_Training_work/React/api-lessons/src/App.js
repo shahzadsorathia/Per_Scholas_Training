6e46715b69702babfb179ca2b55e498e4fdfbd7f
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-08-31 15:38:36 -0400

    Training_Work

--- Module_2_React/Module_Training_work/React/api-lessons/src/App.js
+++ Module_2_React/Module_Training_work/React/api-lessons/src/App.js
@@ -32,18 +32,18 @@ usually anything hat will depend on time can be async
 // EXAMPLE OF ASYNCHRONOUS - setTimeout, Promises, FETCH
 
 // First argument is a function, second argument is the time lapse before the code runs
-setTimeout(() =>{
-  console.log("Asynchronous")
-},3000)
-console.log("HI")
-console.log("BYE")
+// setTimeout(() =>{
+//   console.log("Asynchronous")
+// },3000)
+// console.log("HI")
+// console.log("BYE")
 
-// ===== API =====
+// // ===== API =====
 /*
 
 Application Programming Interface: collection fo clearly defined methods of communication between different software components. Allows us to retrieve data from a system aka (database) and use it in our application.
 
-TLDR: APIs - DOM, Fetc, Third-party APIs
+TLDR: APIs acts as a messenger to send HTTP requests to a server and gives us a response.
 
 YouTube: https://youtu.be/s7wmiS2mSXY
 
@@ -54,9 +54,12 @@ YouTube: https://youtu.be/s7wmiS2mSXY
 // ===== JSON =====
 /*
 
- Above, we talked about API returning to us a response. One of the most common ways for APIs to store, structure and transfer data is through JSON.
+ Above, we talked about API returning to us a response. 
+ One of the most common ways for APIs to store, structure and 
+ transfer data is through JSON.
 
- JSON: 
+ JSON: JavaScript Object Notation. Just like a object,
+ it has key/value pairs which can take in different data types.
 
  Format of an object:
  {
@@ -64,6 +67,10 @@ YouTube: https://youtu.be/s7wmiS2mSXY
    "age": "forever 23"
  }
 
+ format of typical JavaScript Objects:
+ {
+   name: "Teo"
+ }
 */
 
 // ===== HTTP PROTOCOL =====
@@ -72,9 +79,10 @@ YouTube: https://youtu.be/s7wmiS2mSXY
  Picture example:
  Mod-1 -> Week 4 -> Day 2 -> slide: 9 
 
- HTTP Protocol: 
+ HTTP Protocol: used to delver data (HTML Files, Images, Etc) on the internet
 
- TLDR: 
+ TLDR: Transmission Control Protocol and Internet Protocol - protocols that work together
+ to make sure data has a destination and get's there safely.
 
  For more details:
  TCP/IP: https://www.fortinet.com/resources/cyberglossary/tcp-ip
@@ -90,8 +98,8 @@ YouTube: https://youtu.be/s7wmiS2mSXY
 
  For the sake of this course, we will be focusing on REST using JSON data.
 
- SOAP: 
- REST: 
+ SOAP: Simple Object Access Protocol - you send a whole XML file.
+ REST: Representional State Transfer - you send a representational data as JSON object.
 
 */
 
@@ -101,9 +109,9 @@ YouTube: https://youtu.be/s7wmiS2mSXY
  Picture example:
  Mod-1 -> Week 4 -> Day 2 -> slide: 11
 
- URI:
+ URI: Unifrom Resource Identifier
 
- Purpose:
+ Purpose: Locate resources/access web pages on the server hosting the web services
 
  Format:
  <protocol>://<service-name>/<ResourceType>/<ResourceID>
@@ -126,13 +134,20 @@ YouTube: https://youtu.be/s7wmiS2mSXY
  ======================================
               IMPORTANT
  ======================================
- CRUD cycle:
+ CRUD cycle: Vreat, Read, Update, and Delete. 
+ Most of the things that we do lnline uses these basic operations of 
+ PERSISTENT storage. FOREVER!!
+ Remember everything that we are seeing online is data that is probably saved in a database.
+
+ Examples of things we can CRUD: files, personal information on a website,
+ instagram photos, gaming profiles.
 
  Examples of things we can CRUD: 
 
- HTTP verbs: 
+ HTTP verbs: Five verbs that include GET, POST, PUT, and DELETE. the one will be working on in this course will be 
+ GET, PUT, POST, DELETE.
 
- CRUD | HTTP VERB
+ CRUD | HTTP VERB ties in with HTTP request.
  ----------------
  Create - POST
  Read - GET
@@ -146,19 +161,19 @@ YouTube: https://youtu.be/s7wmiS2mSXY
 
 // ===== AJAX, PROMISES, FETCH, ASYNC/AWAIT =====
 
-// AJAX: 
-// AJAX calls: 
+// AJAX: stands for Asynchronous JavaScript and XML - which allows us to update parts of a web page without reloading
+// AJAX calls: allows us to do HTTP requests with a server / database without the need for a refresh
 
-// Fetch: allows us to do AJAX calls which returns a Promise
+// Fetch: allows us to do AJAX calls which returns a Promise 
 
 // ===== PROMISE =====
 /*
 
  Promises has three states:
  
- Pending: 
- fulfilled: 
- rejected: 
+ Pending: initial state, neither fulfilled or rejected 
+ fulfilled: successfull
+ rejected: something went wrong
 
 */
 
@@ -169,17 +184,119 @@ YouTube: https://youtu.be/s7wmiS2mSXY
 // Since we don't know when promises will return to us a response, it has built in methods such as .then() that does more things once we get a response
 
 // Show example of under the hood:
+const promiseToDoSomething = new Promise((resolve, rejected) => {
+  // there will be code here that returns a response
+  let something = true
+  if (something === true) {
+    resolve("Success")
+  } else {
+    rejected("Failed")
+  }
+})
+
+promiseToDoSomething
+  .then()
+  .then()
+  .catch()
 
 // For the sake of us using Fetch, it will just return a Promise object to us that we can use
 
 class App extends Component {
+
+  // GetData = () => {
+  //   //console.log("We are inside GetData")
+  //   //fetch - first argument takes in the URL
+  //   //Remeber Fetch returns a PRMISE
+  //   // HTTP GET REQUEST
+
+  //   fetch('https://jsonplaceholder.typicode.com/todos/1') // retruns a PROMISE
+  //     // After we get a PROMISE from fetch() we run the code below
+  //     // The way .then() works its it takes a callback .. a function within a function
+  //     // The Object response that we got is not readbale for us yet 
+  //     // The way we TRANSLATE our response to javascript is to use the method JSON()
+
+  //     .then(Response => Response.json())
+  //     //after we use json() we can THEN usa .then() to do the next task
+  //     .then(data => console.log(data))
+  
+
+  // THIRD PARTY API - Google Twitter ... companies provides APIS which allows us access to their data.
+  //A lot of FREE APIs and some require API key
+
+  // ======= API KEY =======
+  // to open API door -> accessing their data
+  // API keys are tere for there for companies to keep track of how you use their data.
+  // so you dont abuse it.
+
+  //====== GOALS =======
+  // 1.) Get an API key from OMDB
+  // 2.) Write a fetch call to get a Movie based on what we typed.
+  // 3.) In our app we will have an input field where we can type a movie title and render that movie 
+  // in our application.
+
+  // STATE - new syntax -  HIGHLY RECOMMENDED
+  // What our URL query should look like https://www.omdbapi.com/?apikey=bb39feaa&t=toystory
+  //==== BASIC URL QUIERIES ====
+  // The ? start our query parameters
+  // The & is used as a comma when using multiple parameters
+
+  state = {
+    movieTitle: ' ',
+    baseUrl: "http://www.omdbapi.com/?",
+    apiKey: "apikey=bb39feaa",
+    query: "&t=",
+    // searchURL is a combination of baseURL, apiKey, query, and movieTitle
+    searchURL: " ",
+    movieData: {}
+  }
+
+  handleChange = (event) => {
+    console.log("Searching for this title: ", event.target.value)
+
+    this.setState({
+      movieTitle: event.target.value
+    })
+  }
+
+  handleSubmit = (event) =>{
+    // default behavior - web pages refreshes when we do a submit
+    // must do in form when handling submit
+
+    event.preventDefault()
+    console.log("We are inside handleSubmit.")
+    console.log(
+        this.state.baseUrl 
+      + this.state.apiKey 
+      + this.state.query 
+      + this.state.movieTitle
+    )
+//this.setState CAN actually take in TWO arguments
+// frist argument is an object with us updating our state
+    this.setState({
+      searchURL: 
+        this.state.baseUrl 
+      + this.state.apiKey 
+      + this.state.query 
+      + this.state.movieTitle
+    })
+  }
+
   render() {
     return (
       <div >
         <div id="header" >APIs and Everything in Between</div>
+        {/* <button onClick={this.GetData}> Fetch </button> */}
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="title">Title:  </label>
+          <input
+            type="text"
+            id="title"
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
 }
-
 export default App;
