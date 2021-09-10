63e9bbc6ff3497a9a806c6fce28e4553384fe9cd
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-09-10 01:27:06 -0400

    Update

--- Module_2_React/Module_Training_work/React/React-Students-w3d2-hw/src/Student.js
+++ Module_2_React/Module_Training_work/React/React-Students-w3d2-hw/src/Student.js
@@ -2,7 +2,7 @@ import Score from "./Score"
 const Student =(props) => {
     const scoreList = props.scholars.scores.map((score)=> <Score points={score}/>)
     return (
-        <div>
+        <div >
             <div>name: {props.scholars.name} </div>
            <div>bio: {props.scholars.bio}</div>
            {scoreList} 
