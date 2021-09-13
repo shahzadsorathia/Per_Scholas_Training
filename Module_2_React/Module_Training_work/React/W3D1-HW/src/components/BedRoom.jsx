c0dc22f73a1fe054ae88ed39a1b0fc6f9f2ffaad
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-09-13 14:58:42 -0400

    Update BedRoom.jsx

--- Module_2_React/Module_Training_work/React/W3D1-HW/src/components/BedRoom.jsx
+++ Module_2_React/Module_Training_work/React/W3D1-HW/src/components/BedRoom.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const BedRoom = (props) => {
     return (
-        <div className='bedroom' id={`bed-${props.bedNum}`}>
-        Bedroom {props.bedNum} 
+        <div className='bedroom'
+            id={`bed-${props.bedNum}`}>
+            Bedroom {props.bedNum}
         </div>
     )
 }
