2ea46655a581c5a7516cef6a25533cdc0bdb99cf
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-09-13 15:00:55 -0400

    Update Bath.jsx

--- Module_2_React/Module_Training_work/React/W3D1-HW/src/components/Bath.jsx
+++ Module_2_React/Module_Training_work/React/W3D1-HW/src/components/Bath.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const Bath = (props) => {
     return (
+<<<<<<< HEAD
         <div className='both'
             id={`bath-${props.size}`}>
             {props.size} Bath
         </div>
+=======
+        <div className='both' 
+        id={`bath-${props.size}`}>
+             {props.size} Bath
+            </div>
+>>>>>>> 64cfaafc6c0630d5d993c5a18ea0a94a5effc4e3
     )
 }
 
