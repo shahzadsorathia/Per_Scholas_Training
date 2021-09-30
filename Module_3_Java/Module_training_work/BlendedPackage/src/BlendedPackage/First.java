9664af3fb7fa7f1e19288bbd4d07158b60f5a445
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-09-30 12:31:52 -0400

    Create First.java

---
+++ Module_3_Java/Module_training_work/BlendedPackage/src/BlendedPackage/First.java
@@ -0,0 +1,10 @@
+package BlendedPackage;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My First java Program...");
+	}
+
+}
