ae9f815b21cf1632bbc79dd230bccafb6c6ad8de
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-09-30 02:00:00 -0400

    Create store_number.java

---
+++ Module_3_Java/Week1_HW1 (Variables & Conditions)/src/store_number/store_number.java
@@ -0,0 +1,28 @@
+package store_number;
+
+   public static void main(String args[]) {
+   	
+int num =22;
+
+
+System.out.print("  ");
+
+if ( num % 2 != 0 ){
+        System.out.println("Cool");
+}
+
+if ( num % 2 == 0 && num >=2 && num <= 5 ) {
+        System.out.println("Not Cool");
+}
+
+if ( num % 2 == 0 && num >=6 && num <= 20 ) {
+        System.out.println("Cool");
+}
+
+if ( num % 2 == 0 && num >= 20 ) {
+        System.out.println("Not Cool");
+}
+
+}
+	
+}
